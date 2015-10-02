41b41f202c14bf8121608af76a0c18279d680310
Travis Jette <travisjette@wl-85-100.wireless.umt.edu>
2015-10-02 12:10:47 -0600

    second draft

--- click.js
+++ click.js
@@ -1,1 +1,16 @@
-function clicker(counter,)
+ var a = function(start, counter) {
+
+this.start = start;
+this.counter = counter;
+this.math = function;
+}
+
+function countSum(){
+var clicks = 0 + this.counter;
+return clicks;
+
+}
+
+var clicker = new a (0, 1);
+
+clicker.math();
