b287ab29e8522d540ef2a772b83a7fa48d486f71
tjette <travisjette@gmail.com>
2015-10-21 15:48:10 -0600

    blog

--- model/post.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var blogSchema   = new Schema({
-    title: String
-    body: String
-});
-
-module.exports = mongoose.model('blog', blogSchema);
