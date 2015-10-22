52467602e469472f5409525196cac7b50b0e0991
Travis Jette <travisjette@wl-87-190.wireless.umt.edu>
2015-10-22 13:40:59 -0600

    passport practice

--- server.js
+++ server.js
@@ -6,12 +6,43 @@ var bodyParser = require('body-parser');
 var db = require('./model/db');
 var blogModel = require('./model/blog');
 var blogRoutes = require('./routes/blog');
+var mongoose = require('mongoose');
 
+
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
-app.use(express.static('public'));
-app.use('/api/blog', blogRoutes);
-app.get('/', function(req, res){
-    res.readFile('index.html')
-});
-var port = process.env.PORT || 8080;
+
+var configDB = require('./config/database.js');
+
+// mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./routes/userRoutes')(app, passport); // load our routes and pass in our app and fully configured passport
+var port = process.env.PORT || 4000;
+
 app.listen(port); 
+console.log("listening on port 4000");
+
+// app.use(express.static('public'));
+// app.use('/api/blog', blogRoutes);
+// app.get('/', function(req, res){
+//     res.readFile('index.html')
+
